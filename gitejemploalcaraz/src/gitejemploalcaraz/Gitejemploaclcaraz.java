1972847079e3789b93d759d82f34f4e6ce5680b7
Xavier-Alcaraz <xavieralcaraz01@gmail.com>
2021-02-24 16:10:51 +0100

    Version 0.0.1.

---
+++ gitejemploalcaraz/src/gitejemploalcaraz/Gitejemploaclcaraz.java
@@ -0,0 +1,34 @@
+/*
+//19/02/2021
+//proyecto practica alcaraz
+ */
+package gitejemploalcaraz;
+
+/**
+ *
+ * @author Xavier
+ */
+public class Gitejemploaclcaraz  {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int day=28, month=5, year=2001;
+        String stDat=birthday_zeller(day, month, year);
+        System.out.println("Mi cumpleaños: " + stDat + " " + day + "/" + month + "/" + year);
+        
+    }
+
+    private static String birthday_zeller(int day, int month, int year) {
+        String[] ArrayWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        int a, y, m, d;
+        a = (14 - month) / 12;
+        y = year - a;
+        m = month + 12 * a - 2;
+        d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+        return ArrayWeek[d];
+    }
+    
+}
+
